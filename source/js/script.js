0a9c127e332754d4626aa247a4401fb5ca986e50
alexeioparin <60060422+alexeioparin@users.noreply.github.com>
2020-08-19 22:53:04 +0300

    Верстка десктопной версии

--- source/js/script.js
+++ source/js/script.js
@@ -19,3 +19,26 @@ let headerMenu = document.querySelector('.main-header__list');
 menuButton.addEventListener('click', function() {
   headerMenu.classList.toggle('visually-hidden');
 })
+
+let mailLinkButton = document.querySelector('.main-header__mail-link');
+let mailCopyButton = document.querySelector('.main-header__mail-copy');
+
+mailLinkButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  mailCopyButton.classList.toggle('visually-hidden');
+})
+
+let phoneLink = document.querySelector('.main-header__list-item--phone');
+let phoneMenu = document.querySelector('.main-header__phone-menu');
+let phoneMenuCloseButton = document.querySelector('.main-header__phone-menu>span');
+let onButtonClosePhoneMenu = function () {
+  phoneMenu.classList.add('visually-hidden');
+  phoneMenuCloseButton.removeEventListener('click', onButtonClosePhoneMenu);
+  phoneLink.removeEventListener('mouseout', onButtonClosePhoneMenu);
+};
+
+phoneLink.addEventListener('mouseover', function() {
+  phoneMenu.classList.remove('visually-hidden');
+  phoneLink.addEventListener('mouseout', onButtonClosePhoneMenu);
+  phoneMenuCloseButton.addEventListener('click', onButtonClosePhoneMenu);
+})
